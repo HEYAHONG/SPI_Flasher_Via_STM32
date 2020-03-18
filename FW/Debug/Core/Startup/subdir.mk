5f8e9b81df27c88b0be3242deca8d487b913f903
HYH <2229388563@qq.com>
2020-03-18 15:36:30 +0800

    代码备份

--- FW/Debug/Core/Startup/subdir.mk
+++ FW/Debug/Core/Startup/subdir.mk
@@ -12,5 +12,5 @@ OBJS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -I"D:/windows_10_software/Users/Administrator/Desktop/SPI_Flasher_Via_STM32/FW/APP" -x assembler-with-cpp --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -I"D:/windows_10_software/Users/Administrator/Desktop/SPI_Flasher_Via_STM32/FW/APP" -I"D:/windows_10_software/Users/Administrator/Desktop/SPI_Flasher_Via_STM32/FW/3rdparty/EEPROM24xx" -I"D:/windows_10_software/Users/Administrator/Desktop/SPI_Flasher_Via_STM32/FW/3rdparty/w25qxx" -x assembler-with-cpp --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 
